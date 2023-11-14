26ee48e8e4ad4a2abc37d9d8e936182b905322b4
Rafou98 <95037521+Rafou28@users.noreply.github.com>
2023-11-14 09:25:56 +0100

    Les 5 premiers plus les nombres paires

--- labo3/student_DOUSSE_SURDEZ_TRANCHIDA.sql
+++ labo3/student_DOUSSE_SURDEZ_TRANCHIDA.sql
@@ -1,9 +1,10 @@
 SET search_path = pagila;
 
+
 -- BEGIN Exercice 01
-SELECT customer_id,
-       last_name,
-       email
+SELECT customer_id as id_client,
+       last_name   as nom,
+       email       as e_mail
 FROM customer AS C
 WHERE C.first_name = 'PHYLLIS'
   AND store_id = 1
@@ -12,8 +13,8 @@ ORDER BY C.customer_id DESC;
 
 
 -- BEGIN Exercice 02
-SELECT title,
-       release_year
+SELECT title        as titre,
+       release_year as annee_de_sortie
 FROM film as F
 WHERE f.rating = 'R'
   AND f.length < 60
@@ -23,9 +24,9 @@ ORDER BY title ASC;
 
 
 -- BEGIN Exercice 03
-SELECT address,
-       city,
-       country
+SELECT address as adresse,
+       city    as ville,
+       country as pays
 FROM address
          JOIN city on address.city_id = city.city_id
          JOIN country c on city.country_id = c.country_id
@@ -39,28 +40,52 @@ ORDER BY country ASC,
 -- BEGIN Exercice 04
 -- Listez tous les clients actifs (customer_id, prenom, nom) habitant la ville 171, et rattachés au
 -- magasin numéro 1. Triez-les par ordre alphabétique des prénoms.
-SELECT *
+SELECT customer_id as id_client, first_name as prenom, last_name as nom
 FROM customer
+         JOIN address on customer.address_id = address.address_id
+    AND city_id = 171
 WHERE active = TRUE
-    AND address_id IN (SELECT address_id
-                         FROM address
-                         WHERE city_id = 171)
-    AND store_id = 1
+  AND store_id = 1
 ORDER BY first_name ASC;
+
 -- END Exercice 04
 
-
 -- BEGIN Exercice 05
---Donnez le nom et le prénom (prenom_1, nom_1, prenom_2, nom_2) des clients qui ont loué au
---moins une fois le même film (par exemple, si ALAN et BEN ont loué le film MATRIX, mais pas TRACY,
---seuls ALAN et BEN doivent être listés).
+-- Donnez le nom et le prénom (prenom_1, nom_1, prenom_2, nom_2) des clients qui ont loué au
+-- moins une fois le même film (par exemple, si ALAN et BEN ont loué le film MATRIX, mais pas TRACY,
+-- seuls ALAN et BEN doivent être listés).
+SELECT c1.first_name AS prenom1,
+       c1.last_name  AS nom1,
+       c2.first_name AS prenom2,
+       c2.last_name  AS nom2
+
+FROM customer AS c1
+         JOIN rental r1 on c1.customer_id = r1.customer_id
+         JOIN inventory i1 on r1.inventory_id = i1.inventory_id
+         JOIN inventory i2 on i2.film_id = i1.film_id
+         JOIN rental r2 on i2.inventory_id = r2.inventory_id
+         JOIN customer c2 on r2.customer_id = c2.customer_id
+WHERE c1.customer_id < c2.customer_id;
+
 -- END Exercice 05
 
-
-
+--Donnez le nom et le prénom des acteurs (nom, prenom) ayant joué dans un film d’horreur, dont le
+--prénom commence par K, ou dont le nom de famille commence par D sans utiliser le mot clé JOIN.
 -- BEGIN Exercice 06
 -- END Exercice 06
 
+SELECT first_name as prenom, last_name as nom
+FROM actor
+WHERE first_name LIKE 'K%'
+   OR last_name LIKE 'D%'
+    AND actor_id =
+        ANY (SELECT actor_id
+             FROM film_actor
+             WHERE film_id = ANY (SELECT film_id
+                                  FROM film_category
+                                  WHERE category_id = ANY (SELECT category_id
+                                                           FROM category
+                                                           WHERE name = 'Horror')));
 
 -- BEGIN Exercice 07a
 -- END Exercice 07a
@@ -68,30 +93,88 @@ ORDER BY first_name ASC;
 -- BEGIN Exercice 07b
 -- END Exercice 07b
 
+-- Donnez la liste des clients (id, nom, prenom) espagnols qui n’ont pas encore rendu tous les films
+-- qu’ils ont empruntés, en les ordonnant par nom. Donnez 3 versions de cette requête :
+-- (a) En utilisant EXISTS (pas de GROUP BY, ni de IN ou NOT IN).
+-- (b) En utilisant IN (pas de GROUP BY, ni de EXISTS ou NOT EXISTS).
+-- (c) En utilisant aucun des mot-clés précédent (c’est à dire pas de GROUP BY, IN, NOT IN, EXISTS, NOT EXISTS).
 
 -- BEGIN Exercice 08a
+-- Reponse différente que la correction de l'assistant mais si on fait le select que sur l'interrieur de la requete,
+-- on remarque que ils sont bien tous en Espagne et qu'ils ont bien des films non rendu... donc normalement ça devrait
+-- être bon
+SELECT customer.customer_id as id_client,
+       customer.last_name   as nom,
+       customer.first_name  as prenom
+FROM customer
+WHERE EXISTS (SELECT *
+              FROM address
+                       JOIN city ON address.city_id = city.city_id
+                       JOIN country ON city.country_id = country.country_id
+                       JOIN rental ON customer.customer_id = rental.customer_id
+              WHERE country.country = 'Spain'
+                AND rental.return_date IS NULL
+                AND address.address_id = customer.address_id)
+ORDER BY customer.last_name ASC;
 -- END Exercice 08a
 
 -- BEGIN Exercice 08b
+SELECT customer.customer_id as id_client, customer.last_name as nom, customer.first_name as prenom
+FROM customer
+         JOIN address ON customer.address_id = address.address_id
+         JOIN city ON address.city_id = city.city_id
+         JOIN country ON city.country_id = country.country_id
+WHERE country.country = 'Spain'
+  AND customer.customer_id IN (SELECT rental.customer_id
+                               FROM rental
+                               WHERE rental.return_date IS NULL)
+ORDER BY customer.last_name ASC;
+
 -- END Exercice 08b
 
 -- BEGIN Exercice 08c
+SELECT customer.customer_id as id_client, customer.last_name as nom, customer.first_name as prenom
+FROM customer
+         JOIN address ON customer.address_id = address.address_id
+         JOIN city ON address.city_id = city.city_id
+         JOIN country ON city.country_id = country.country_id
+WHERE country.country = 'Spain'
+  AND customer.customer_id = ANY (SELECT rental.customer_id
+                                  FROM rental
+                                  WHERE rental.return_date IS NULL)
+ORDER BY customer.last_name ASC;
 -- END Exercice 08c
-
 
 -- BEGIN Exercice 09 (Bonus)
 -- END Exercice 09 (Bonus)
 
-
+-- Donnez le titre des films et le nombre d’acteurs (titre, nb_acteurs) des films dramatiques en les
+-- triant par le nombre d’acteur décroissant. Retenez uniquement les films avec moins de 5 acteurs
 -- BEGIN Exercice 10
+
+SELECT film.title as titre, COUNT(actor.actor_id) AS nombres_acteurs
+FROM film_category
+         JOIN category ON category.category_id = film_category.category_id
+         JOIN film ON film.film_id = film_category.film_id
+         JOIN film_actor ON film.film_id = film_actor.film_id
+         JOIN actor ON film_actor.actor_id = actor.actor_id
+WHERE category.name = 'Drama'
+GROUP BY film.film_id, film.title
+HAVING count(actor.actor_id) < 5
+ORDER BY nombres_acteurs DESC;
 -- END Exercice 10
 
 
 -- BEGIN Exercice 11
 -- END Exercice 11
 
-
+--Affichez le(s) film(s) (id, titre, duree) ayant la durée la moins longue. Si plusieurs films ont la
+--même durée (la moins longue), il faut afficher l’ensemble de ces derniers.
 -- BEGIN Exercice 12
+SELECT film_id as id_film, title as titre, length as longueur
+FROM film
+WHERE length = (SELECT MIN(length)
+                FROM film);
 -- END Exercice 12
 
 
@@ -101,31 +184,116 @@ ORDER BY first_name ASC;
 -- BEGIN Exercice 13b
 -- END Exercice 13b
 
-
+--Un fou furieux décide de regarder l’ensemble des films qui sont présents dans la base de données.
+--Etablissez une requête qui donne le nombre de jours (nb_jours) qu’il devra y consacrer sachant
+--qu’il dispose de 8 h par jour.
 -- BEGIN Exercice 14
+SELECT CEIL(SUM(film.length) / 60 / 8) as nombre_de_jours
+FROM film;
 -- END Exercice 14
 
 
 -- BEGIN Exercice 15
 -- END Exercice 15
 
-
+-- Donnez les 3 requêtes suivantes ainsi que le résultat de la première et de la dernière :
+-- (a) Comptez les paiements dont la valeur est inférieure ou égale à 9.
+-- (b) Effacez ces mêmes paiements.
+-- (c) Comptez à nouveau ces mêmes paiements pour vérifier que l’opération a bien eu lieu.
 -- BEGIN Exercice 16a
+
+SELECT COUNT(*) AS nombre_de_paiements
+FROM payment
+WHERE amount <= 9;
+-- RESULTAT: 15'678
 -- END Exercice 16a
 
 -- BEGIN Exercice 16b
+DELETE
+FROM payment
+WHERE amount <= 9;
 -- END Exercice 16b
 
 -- BEGIN Exercice 16c
+SELECT *
+FROM payment
+WHERE amount <= 9;
+-- RESULTAT: 0
+
+-- COMMENT FAIRE POUR RAJOUTER LES ELEMENTS SUPPRIMER PARCE QUE QUAND JE RELANCE LE SCRIPTE CA MET PAS LES
+-- RESULTATS DANS LA TABLE PAYMENT!!
+
 -- END Exercice 16c
 
 
 -- BEGIN Exercice 17
 -- END Exercice 17
 
+--Un nouveau client possèdant les informations suivantes souhaite louer des films :
+--M. Guillaume Ransome
+--Adresse : Rue du centre, 1260 Nyon
+--Pays : Suisse / Switzerland
+--Téléphone : 021/360.00.00
+--E-mail : gr@bluewin.ch
+--Ce client est rattaché au magasin 1.
+--Insérez-le dans la base de données, avec toutes les informations requises pour lui permettre de louer des films.
+--(a) Spécifiez les attributs (colonnes) lors de l’insertion.
+--Indication : plusieurs requêtes sont nécessaires
+--(b) Pour chaque nouveau tuple, la base de données doit générer l’id. Pourquoi ne pouvez-vous
+--pas le faire ?
+--(c) Pour chaque clé étrangère pour laquelle une valeur est requise, une requête doit donner cette
+--valeur. On considère que l’ensemble des requêtes nécessaires sera fait dans une transaction,
+--ainsi, seules vos modifications de la base de données seront effectives (pas de soucis de
+--concurrence avec une éventuelle autre application).
+--(d) Ecrivez une requête d’interrogation, qui montrera que l’ensemble des opérations s’est bien déroulé.
+--Indication : Guillaume Ransome est un identifiant unique suffisant.
 
--- BEGIN Exercice 18
--- END Exercice 18
+-- BEGIN Exercice 18a
+
+INSERT INTO city (city, country_id, last_update)
+VALUES ('Nyon', (SELECT country_id FROM country WHERE country = 'Switzerland'), NOW());
+
+INSERT INTO address (address, address2, district, city_id, postal_code, phone, last_update)
+VALUES ('Rue du centre', '', 'Vaud', (SELECT city_id FROM city WHERE city = 'Nyon'), '1260', '021/360.00.00', NOW());
+
+INSERT INTO customer (store_id, first_name, last_name, email, address_id, active, create_date, last_update)
+VALUES (1,
+        'Guillaume',
+        'Ransome',
+        'gr@bluewin.ch',
+        (SELECT address_id FROM address WHERE address = 'Rue du centre'),
+        true,
+        NOW(),
+        NOW());
+INSERT INTO customer (store_id, first_name, last_name, email, address_id, active, create_date, last_update)
+VALUES (1,
+        'Rafael',
+        'Dousse',
+        'gr@bluewin.ch',
+        (SELECT address_id FROM address WHERE address = 'Rue du centre'),
+        true,
+        NOW(),
+        NOW());
+
+-- END Exercice 18a
+
+-- BEGIN Exercice 18b
+-- Ici, quand on inserte une nouvelle personne dans la base de donnée, une clé primaire est générée automatiquement
+-- c'est à dire qu'elle est incrémenté à chaque fois qu'une nouvelle donnée est ajouté. On ne peut pas le faire nous
+-- même dans des valeurs qui existe déjà, ce qui est normal vu que la clé doit être unique. Par contre, on peut rajouter
+-- une donnée avec une id plus haute que celle qui existe déjà
+-- END Exercice 18b
+
+-- BEGIN Exercice 18c
+-- JE SAIS PAS
+
+-- END Exercice 18c
 
 -- BEGIN Exercice 18d
+-- Je sais pas si c'est ça qu'on veut?
+SELECT *
+FROM customer
+WHERE first_name = 'Guillaume'
+  AND last_name = 'Ransome';
 -- END Exercice 18d
+

ed17492aff2ff06d33ccdda5a68ada1b3a508bb5
Sainane <47831195+Sainane@users.noreply.github.com>
2023-11-18 12:56:16 +0100

    Exercice impaires, problème 7a et b, a donne 36 et b donne 37 résultats

--- labo3/student_DOUSSE_SURDEZ_TRANCHIDA.sql
+++ labo3/student_DOUSSE_SURDEZ_TRANCHIDA.sql
@@ -87,10 +87,33 @@ WHERE first_name LIKE 'K%'
                                                            FROM category
                                                            WHERE name = 'Horror')));
 
--- BEGIN Exercice 07a
--- END Exercice 07a
+
+
+-- Première façon : Utiliser NOT EXISTS
+SELECT
+    f.film_id,
+    f.title,
+    f.rental_rate/rental_duration as rental_rate_per_day
+FROM film f
+WHERE f.rental_rate/rental_duration <= 1.00
+    AND NOT EXISTS (
+        SELECT 1
+        FROM rental r
+        JOIN inventory i ON r.inventory_id = i.inventory_id
+        WHERE i.film_id = f.film_id
+    );
 
 -- BEGIN Exercice 07b
+    -- Deuxième façon : Utiliser LEFT JOIN et vérifier si il n'y a pas de correspondance (NULL)
+SELECT
+    f.film_id,
+    f.title,
+    f.rental_rate/rental_duration as rental_rate_per_day
+FROM film f
+LEFT JOIN  inventory i ON f.film_id = i.film_id
+LEFT JOIN rental r ON i.inventory_id = r.inventory_id
+WHERE f.rental_rate/rental_duration <= 1.00 AND r.rental_id IS NULL;
+
 -- END Exercice 07b
 
 -- Donnez la liste des clients (id, nom, prenom) espagnols qui n’ont pas encore rendu tous les films
@@ -146,6 +169,29 @@ ORDER BY customer.last_name ASC;
 -- END Exercice 08c
 
 -- BEGIN Exercice 09 (Bonus)
+    -- Sous-requête pour obtenir la liste des films de l'actrice EMILY DEE
+WITH emily_dee_films AS (
+    SELECT DISTINCT
+        fa.film_id
+    FROM actor a
+    JOIN film_actor fa ON a.actor_id = fa.actor_id
+    WHERE a.first_name = 'EMILY' AND a.last_name = 'DEE'
+)
+
+-- Requête principale pour obtenir les clients ayant loué tous les films d'EMILY DEE
+SELECT
+    c.customer_id,
+    c.first_name AS prenom,
+    c.last_name AS nom
+FROM customer c
+JOIN rental r ON c.customer_id = r.customer_id
+JOIN inventory i ON r.inventory_id = i.inventory_id
+JOIN emily_dee_films edf ON i.film_id = edf.film_id
+GROUP BY
+    c.customer_id, c.first_name, c.last_name
+HAVING
+    COUNT(DISTINCT edf.film_id) = (SELECT COUNT(*) FROM emily_dee_films);
+
 -- END Exercice 09 (Bonus)
 
 -- Donnez le titre des films et le nombre d’acteurs (titre, nb_acteurs) des films dramatiques en les
@@ -166,6 +212,18 @@ ORDER BY nombres_acteurs DESC;
 
 
 -- BEGIN Exercice 11
+SELECT
+    c.category_id AS id,
+    c.name AS nom,
+    COUNT(fc.film_id) AS nb_films
+FROM category c
+JOIN film_category fc ON c.category_id = fc.category_id
+GROUP BY
+    c.category_id, c.name
+HAVING
+    COUNT(fc.film_id) > 65
+ORDER BY
+    nb_films DESC;
 -- END Exercice 11
 
 --Affichez le(s) film(s) (id, titre, duree) ayant la durée la moins longue. Si plusieurs films ont la
@@ -179,9 +237,56 @@ WHERE length = (SELECT MIN(length)
 
 
 -- BEGIN Exercice 13a
+-- Sous-requête pour obtenir les acteurs ayant joué dans plus de 40 films
+WITH prolific_actors AS (
+    SELECT
+        fa.actor_id
+    FROM film_actor fa
+    GROUP BY
+        fa.actor_id
+    HAVING COUNT(DISTINCT fa.film_id) > 40
+)
+
+-- Requête principale pour obtenir les films
+SELECT DISTINCT
+    f.film_id AS id,
+    f.title
+FROM film f
+JOIN film_actor fa ON f.film_id = fa.film_id
+WHERE fa.actor_id IN (SELECT actor_id FROM prolific_actors)
+ORDER BY
+    f.title;
+
 -- END Exercice 13a
 
+
 -- BEGIN Exercice 13b
+
+-- Sous-requête pour obtenir les acteurs ayant joué dans plus de 40 films
+WITH prolific_actors AS (
+    SELECT
+        fa.actor_id
+    FROM
+        film_actor fa
+    GROUP BY
+        fa.actor_id
+    HAVING
+        COUNT(DISTINCT fa.film_id) > 40
+)
+
+-- Requête principale pour obtenir les films
+SELECT
+    DISTINCT f.film_id AS id,
+    f.title
+FROM
+    film f
+JOIN
+    film_actor fa ON f.film_id = fa.film_id
+JOIN
+    prolific_actors pa ON fa.actor_id = pa.actor_id
+ORDER BY
+    f.title;
+
 -- END Exercice 13b
 
 --Un fou furieux décide de regarder l’ensemble des films qui sont présents dans la base de données.
@@ -194,6 +299,51 @@ FROM film;
 
 
 -- BEGIN Exercice 15
+    SELECT
+    c.customer_id AS id,
+    c.last_name || ', ' || c.first_name AS nom,
+    c.email,
+    co.country AS pays,
+    COUNT(r.rental_id) AS nb_locations,
+    SUM(p.amount) AS depense_totale,
+    AVG(p.amount) AS depense_moyenne
+FROM
+    customer c
+JOIN rental r ON c.customer_id = r.customer_id
+JOIN payment p ON r.rental_id = p.rental_id
+JOIN staff s ON p.staff_id = s.staff_id
+JOIN address a ON c.address_id = a.address_id
+JOIN city ci ON a.city_id = ci.city_id
+JOIN country co ON ci.country_id = co.country_id
+GROUP BY
+    c.customer_id, c.last_name, c.first_name, c.email, co.country;
+SELECT *
+FROM (
+    -- Sous-requête pour obtenir tous les clients avec leur dépense moyenne
+    SELECT
+        c.customer_id AS id,
+        c.last_name || ', ' || c.first_name AS nom,
+        c.email,
+        co.country AS pays,
+        COUNT(r.rental_id) AS nb_locations,
+        SUM(p.amount) AS depense_totale,
+        AVG(p.amount) AS depense_moyenne
+    FROM customer c
+    JOIN rental r ON c.customer_id = r.customer_id
+    JOIN payment p ON r.rental_id = p.rental_id
+    JOIN staff s ON p.staff_id = s.staff_id
+    JOIN address a ON c.address_id = a.address_id
+    JOIN city ci ON a.city_id = ci.city_id
+    JOIN country co ON ci.country_id = co.country_id
+    GROUP BY
+        c.customer_id, c.last_name, c.first_name, c.email, co.country
+) AS customer_summary
+WHERE
+    pays IN ('Switzerland', 'France', 'Germany')
+    AND depense_moyenne > 3.0
+ORDER BY
+    pays, nom;
+
 -- END Exercice 15
 
 -- Donnez les 3 requêtes suivantes ainsi que le résultat de la première et de la dernière :
@@ -227,6 +377,16 @@ WHERE amount <= 9;
 
 
 -- BEGIN Exercice 17
+    -- (a) Majorer de 50 % les paiements de plus de 4$
+-- (b) Mettre à jour la date de paiement avec la date courante du serveur
+
+UPDATE payment
+SET
+    amount = amount * 1.5,
+    payment_date = CURRENT_TIMESTAMP
+WHERE
+    amount > 4;
+
 -- END Exercice 17
 
 --Un nouveau client possèdant les informations suivantes souhaite louer des films :
